821446e17a96964e08fe4c3d1ce0a760f1366079
ByteMovesTech <kcmike@gmail.com>
2026-03-01 14:35:10 -0600

    Add files via upload

---
+++ js/firebase-config.js
@@ -0,0 +1,9 @@
+// firebase-config.js
+const firebaseConfig = {
+  apiKey: "AIzaSyCNvc-KTRnSrQfU0lIkK9I-t4qSdx1cG4s",
+  authDomain: "cookiecuttervault.firebaseapp.com",
+  projectId: "cookiecuttervault",
+  storageBucket: "cookiecuttervault.firebasestorage.app",
+  messagingSenderId: "900114441261",
+  appId: "1:900114441261:web:fd4f37af79e928933344a4"
+};
